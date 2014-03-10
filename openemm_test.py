0175cef7ee5020b7e488062e42fd2268d2577a9a
Ronoaldo JLP <ronoaldo@gmail.com>
2014-03-10 12:10:35 +0000

    Added new test case for sending campaigns.

--- openemm_test.py
+++ openemm_test.py
@@ -16,6 +16,10 @@
 
 import unittest
 import openemm
+import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 class TestSubscriberMethods(unittest.TestCase):
     """
@@ -47,6 +51,61 @@ class TestSubscriberMethods(unittest.TestCase):
                 self.assertEquals('0', item.value)
                 has_gender = True
         self.assertTrue(has_gender)
+
+class TestCampaignMethods(unittest.TestCase):
+
+    def test_SendCampaign(self):
+        client = openemm.Connect('test', 'test')
+        
+        mailinglist_id = 1
+        sender_name = 'Acme, Inc'
+        sender_address = 'acme@example.com'
+        reply_to_name = 'Customer Care'
+        reply_to_address = 'customercare@example.com'
+        subject = '[News] Great updates from our testing campaign'
+        content = '<h1>Hello, world</h1>'
+        
+        # Create a new mailing
+        mailing_id = client.service.AddMailing(
+            'test-campaign-2014-01-01', # Campaign short name
+            'Sample test campaign', # Campaign description
+            mailinglist_id, # Target mailing list id
+            [], # List of target group ids
+            'all', # Match all target groups
+            'regular', # Regular campaign
+            subject, # Mailing subject
+            sender_name,
+            sender_address,
+            reply_to_name,
+            reply_to_address,
+            'utf-8', # Content charset
+            0, # Automatic linefeed column setup
+            'online-html', # Display as online-html, no attachments
+            'bottom' # Tracking pixel at bottom
+        )
+        logging.info("New mailing added: %s" % mailing_id)
+
+        # Add mailing content
+        content_id = client.service.AddContentBlock(
+            mailing_id,
+            'emailHtml',
+            None,
+            content,
+            1,
+        )
+        logging.info("Added content-block %s" % content_id)
+
+        # Schedule sending for now
+        send_date = datetime.datetime.now() + datetime.timedelta(24)
+        logging.info("Sending new mailing at %s" % send_date)
+        client.service.SendMailing(
+            mailing_id,
+            'world',
+            send_date.strftime('%Y-%m-%d %H:%M:%S GMT'),
+            0, # Block size
+            0  # Use default stepping
+        )
+        logging.info("New mailing schedule to be sent.")
 
 if __name__ == '__main__':
     unittest.main()
